0264d09af1356206718f3560ebd88b5d8d54aeff
julialips <45575431+julialips@users.noreply.github.com>
2019-02-20 19:02:54 +0300

    Update PublicTests.cs

--- FancyCalc/Tests/PublicTests.cs
+++ FancyCalc/Tests/PublicTests.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using NUnit.Framework;
 
 namespace FancyCalc
@@ -14,7 +14,22 @@ namespace FancyCalc
             double expected = 4;
             double actual = calc.Add(2, 2);
             Assert.AreEqual(expected, actual);
+
+            //double expected2 = 6;
+            //double actual2 = calc.Add(3, 3);
+            //Assert.AreEqual(expected2, actual2);
         }
+
+        [Test]
+        public void CalculateTestHidden()
+        {
+            FancyCalcEnguine calc = new FancyCalcEnguine();
+            double expected2 = 6;
+            double actual2 = calc.Add(3, 3);
+            Assert.AreEqual(expected2, actual2);
+        }
+
+      
 
         [Test]
         public void SubtractTest()
@@ -25,12 +40,26 @@ namespace FancyCalc
             Assert.AreEqual(expected, actual);
         }
 
+        [Test]
+        public void SubtractTest2()
+        {
+            var calc = new FancyCalcEnguine();
+            double expected = 1;
+            double actual = calc.Subtract(10, 9);
+            Assert.AreEqual(expected, actual);
+        }
+
+
         [TestCase(3, 3, ExpectedResult = 9)]
         [TestCase(1, 0, ExpectedResult = 0)]
+        [TestCase(1, -1, ExpectedResult = -1)]
+        
         public double MultiplyTest(int a, int b)
         {
             var calc = new FancyCalcEnguine();
             return calc.Multiply(a, b);
         }
+       
+
     }
 }
